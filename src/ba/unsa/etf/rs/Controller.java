5517007b96745629d04695c2b0a29351b9f1cc3b
Ismar <izdralovic2@etf.unsa.ba>
2021-03-23 13:33:03 +0100

    drugi commit prava verzija

---
+++ src/ba/unsa/etf/rs/Controller.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rs;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+
+    public Label helloWorld;
+
+
+    public void onClick(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+        GridPane tata;
+
+    }
+}
